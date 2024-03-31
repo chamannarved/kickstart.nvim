9ba07354da317ea00c023ba3b2f014cacf41079a
Chaman Narved <chamannarved@gmail.com>
2024-03-31 20:19:00 +0530

    Add keymaps for movie lines

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -9,4 +9,12 @@ keymap.set('n', '<leader>l', vim.cmd.Lazy, { desc = 'Open [L]azy UI' })
 -- Explore
 keymap.set('n', '<leader>e', vim.cmd.Explore, { desc = 'Open File [E]xplore' })
 
+-- Move Lines
+keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move Down' })
+keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move Up' })
+keymap.set('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
+keymap.set('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
+keymap.set('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'Move Down' })
+keymap.set('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move Up' })
+
 return {}
