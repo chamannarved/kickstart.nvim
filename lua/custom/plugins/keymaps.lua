e427b37eab1dda12c74be07f7b485a097222b545
Chaman Narved <chamannarved@gmail.com>
2024-03-31 22:16:32 +0530

    Add window resize keymaps to nvim

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -9,6 +9,12 @@ keymap.set('n', '<leader>l', vim.cmd.Lazy, { desc = 'Open [L]azy UI' })
 -- Explore
 keymap.set('n', '<leader>e', vim.cmd.Explore, { desc = 'Open File [E]xplore' })
 
+-- Resize window using <ctrl> arrow keys
+keymap.set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+keymap.set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+keymap.set('n', '<C-Right>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+keymap.set('n', '<C-Left>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
+
 -- Move Lines
 keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move Down' })
 keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move Up' })
